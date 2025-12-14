c9045b2646fea34183c7669f7c97977250f9608b
Kalana <dasunkarunarathna07@gmail.com>
2025-12-14 23:51:13 +0530

    Fix ESLint config for Node and Jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,13 @@
-import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    }
+  }
+];
